d5023fcbc575ce0b2cc9572d29c03bb316e2a74f
chathu2686 <chathu2686@hotmail.com>
2021-12-02 14:16:43 +0000

    completed GET /api

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -1,11 +1,10 @@
 const { fetchEndpoints } = require("../models/api-model");
+const { editReviewById } = require("../models/reviews-model");
 
 exports.getEndpoints = (req, res, next) => {
   fetchEndpoints()
     .then((endPoints) => {
-      //   console.log(endPoints);
-
-      res.status(200).send({ endPoints });
+      res.status(200).send(endPoints);
     })
     .catch((err) => {
       console.log(err);
--- models/api-model.js
+++ models/api-model.js
@@ -1,11 +1,7 @@
 const fs = require("fs/promises");
 
 exports.fetchEndpoints = () => {
-  //   return fs.readFile("../endpoints.json", "utf-8").then((res) => {
-  //     console.log(res);
-
-  return fs.readFile("./endpoints.json").then((result) => {
-    console.log(result.toString());
-    return result.toString();
+  return fs.readFile("./endpoints.json", "utf-8").then((result) => {
+    return JSON.parse(result);
   });
 };
