f4f4ab4dab3098459b3feac1164e55e47b37381f
chathu2686 <chathu2686@hotmail.com>
2021-12-05 16:35:16 +0000

    debug8

--- models/reviews-model.js
+++ models/reviews-model.js
@@ -44,36 +44,34 @@ const isSortByValid = (query) => {
 };
 
 exports.fetchReviews = (category, sortBy, order, limit, page) => {
-  // return Promise.all([
-  //   isValidCategory(category),
-  //   isSortByValid(sortBy),
-  //   isOrderValid(order),
-  //   isIntegerOverZero(limit),
-  //   isIntegerOverZero(page),
-  // ])
-  //   .then(() => {
-  //     // SQLinjection has been blocked through the above functions inside promise.all
-  const editedCategory = category.replaceAll("'", "''");
+  return Promise.all([
+    isValidCategory(category),
+    isSortByValid(sortBy),
+    isOrderValid(order),
+    isIntegerOverZero(limit),
+    isIntegerOverZero(page),
+  ])
+    .then(() => {
+      // SQLinjection has been blocked through the above functions inside promise.all
+      const editedCategory = category.replaceAll("'", "''");
 
-  return (
-    db
-      .query(
+      return db.query(
         `SELECT 
         reviewData.review_id, title, reviewData.designer, reviewData.owner,reviewData.review_img_url, reviewData.category, reviewData.created_at,reviewData.votes, COUNT(commentData.review_id)::INT AS comment_count
         FROM reviewData
         LEFT JOIN commentData ON commentData.review_id = reviewData.review_id
         where reviewData.category LIKE $1
         GROUP BY reviewData.review_id
+        ORDER BY ${sortBy} ${order}
         ;`,
         [editedCategory]
-      )
-      // })
-      .then(({ rows }) => {
-        const result = pagination(rows, Number(limit), Number(page));
+      );
+    })
+    .then(({ rows }) => {
+      const result = pagination(rows, Number(limit), Number(page));
 
-        return { reviews: result, total_count: rows.length };
-      })
-  );
+      return { reviews: result, total_count: rows.length };
+    });
 };
 
 exports.fetchReviewById = (review_id) => {
