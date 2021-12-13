fc6231bc4274e186438471db0106e5bece0b5d87
chathu2686 <chathu2686@hotmail.com>
2021-12-13 14:21:35 +0000

    amended welcome message

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome to Tharaka's game-data API!");
+  res
+    .status(200)
+    .send("Welcome to Tharaka's game-data API deployed through CI!");
 });
 
 app.use("/api", apiRouter);
